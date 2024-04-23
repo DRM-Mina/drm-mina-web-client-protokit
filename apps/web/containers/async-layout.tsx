c31cc289c7cd840e67d144b906fdd2cd9ea82677
kadirchan <kadircanbozkurt1905@gmail.com>
2024-04-23 22:53:16 +0300

    apply changes to components

--- apps/web/containers/async-layout.tsx
+++ apps/web/containers/async-layout.tsx
@@ -5,11 +5,10 @@ import { Toaster } from "@/components/ui/toaster";
 import { useBalancesStore, useObserveBalance } from "@/lib/stores/balances";
 import { useChainStore, usePollBlockHeight } from "@/lib/stores/chain";
 import { useClientStore } from "@/lib/stores/client";
-import { useNotifyTransactions, useWalletStore } from "@/lib/stores/wallet";
+import { useNotifyTransactions } from "@/lib/stores/transactionStore";
 import { ReactNode, useEffect, useMemo } from "react";
 
 export default function AsyncLayout({ children }: { children: ReactNode }) {
-  const wallet = useWalletStore();
   const client = useClientStore();
   const chain = useChainStore();
   const balances = useBalancesStore();
@@ -20,11 +19,6 @@ export default function AsyncLayout({ children }: { children: ReactNode }) {
 
   useEffect(() => {
     client.start();
-  }, []);
-
-  useEffect(() => {
-    wallet.initializeWallet();
-    wallet.observeWalletChange();
   }, []);
 
   const loading = useMemo(
