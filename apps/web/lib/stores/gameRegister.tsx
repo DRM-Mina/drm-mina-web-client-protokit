f4727d00d59fcaff50668587de718d448dd90059
kadirchan <kadircanbozkurt1905@gmail.com>
2024-06-17 21:29:31 +0300

    impl observe games and list

--- apps/web/lib/stores/gameRegister.tsx
+++ apps/web/lib/stores/gameRegister.tsx
@@ -5,11 +5,26 @@ import { PendingTransaction, UnsignedTransaction } from "@proto-kit/sequencer";
 import { useUserStore } from "./userWallet";
 import { useTransactionStore } from "./transactionStore";
 import { useToast } from "@/components/ui/use-toast";
-import { useCallback } from "react";
+import { useCallback, useEffect } from "react";
 import { PublicKey } from "o1js";
 import { UInt64 } from "@proto-kit/library";
+import { UserKey } from "chain/dist/GameToken";
+
+export interface registeredGame {
+  gameId: number;
+  price: number;
+  discount: number;
+  timeoutInterval: number;
+  numberOfDevices: number;
+}
 
 export interface GameRegisterState {
+  registeredGames: number[];
+  setRegisteredGames: (games: number[]) => void;
+
+  registeredGameList: registeredGame[];
+  setRegisteredGameList: (games: registeredGame[]) => void;
+
   registerGameOnChain: (
     client: Client,
     address: string,
@@ -32,6 +47,14 @@ export const useRegisterStore = create<
   [["zustand/immer", never]]
 >(
   immer((set) => ({
+    registeredGames: [],
+    setRegisteredGames: (games) =>
+      set((state) => void (state.registeredGames = games)),
+
+    registeredGameList: [],
+    setRegisteredGameList: (gameList) =>
+      set((state) => void (state.registeredGameList = gameList)),
+
     async registerGameOnChain(
       client: Client,
       address: string,
@@ -104,4 +127,78 @@ export const useRegisterGameOnChain = (
     timeoutInterval,
     numberOfDevices,
   ]);
+};
+
+export const useObserveRegisteredGames = () => {
+  const client = useClientStore();
+  const { userPublicKey } = useUserStore();
+  const registerStore = useRegisterStore();
+  const transactions = useTransactionStore();
+
+  useEffect(() => {
+    if (!client.client || !userPublicKey) return;
+
+    (async () => {
+      const totalGames =
+        await client.client!.query.runtime.GameToken.totalGameNumber.get();
+      const gameIds = Array.from(
+        { length: Number(totalGames?.toString()) },
+        (_, i) => i + 1,
+      );
+      let registeredGames: number[] = [];
+
+      for (const gameId of gameIds) {
+        const publisherAddress =
+          await client.client!.query.runtime.GameToken.publisher.get(
+            UInt64.from(gameId),
+          );
+        if (publisherAddress?.toBase58() === userPublicKey) {
+          registeredGames.push(gameId - 1);
+        }
+      }
+      registerStore.setRegisteredGames(registeredGames);
+    })();
+  }, [client.client, transactions.pendingTransactions, userPublicKey]);
+};
+
+export const useObserveRegisteredGameList = () => {
+  const client = useClientStore();
+  const registerStore = useRegisterStore();
+  const { registeredGames } = registerStore;
+
+  useEffect(() => {
+    (async () => {
+      let games: registeredGame[] = [];
+      for (const gameId of registeredGames) {
+        const price =
+          await client.client!.query.runtime.GameToken.gamePrice.get(
+            UInt64.from(gameId),
+          );
+
+        const discount =
+          await client.client!.query.runtime.GameToken.discount.get(
+            UInt64.from(gameId),
+          );
+
+        const timeoutInterval =
+          await client.client!.query.runtime.GameToken.timeoutInterval.get(
+            UInt64.from(gameId),
+          );
+
+        const numberOfDevices =
+          await client.client!.query.runtime.GameToken.number_of_devices_allowed.get(
+            UInt64.from(gameId),
+          );
+
+        games.push({
+          gameId: gameId,
+          price: Number(price?.toString()),
+          discount: Number(discount?.toString()),
+          timeoutInterval: Number(timeoutInterval?.toString()),
+          numberOfDevices: Number(numberOfDevices?.toString()),
+        });
+      }
+      registerStore.setRegisteredGameList(games);
+    })();
+  }, [registeredGames]);
 };
