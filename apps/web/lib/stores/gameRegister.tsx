3041ccf21736f7c538afbea18c28550fe0ece088
kadirchan <kadircanbozkurt1905@gmail.com>
2024-06-18 03:41:26 +0300

    add methods to register store

--- apps/web/lib/stores/gameRegister.tsx
+++ apps/web/lib/stores/gameRegister.tsx
@@ -8,7 +8,6 @@ import { useToast } from "@/components/ui/use-toast";
 import { useCallback, useEffect } from "react";
 import { PublicKey } from "o1js";
 import { UInt64 } from "@proto-kit/library";
-import { UserKey } from "chain/dist/GameToken";
 
 export interface registeredGame {
   gameId: number;
@@ -32,6 +31,34 @@ export interface GameRegisterState {
     discount: number,
     timeoutInterval: number,
     number_of_devices_allowed: number,
+  ) => Promise<PendingTransaction>;
+
+  changePriceOnChain: (
+    client: Client,
+    address: string,
+    gameId: number,
+    price: number,
+  ) => Promise<PendingTransaction>;
+
+  changeDiscountOnChain: (
+    client: Client,
+    address: string,
+    gameId: number,
+    discount: number,
+  ) => Promise<PendingTransaction>;
+
+  changeTimeoutIntervalOnChain: (
+    client: Client,
+    address: string,
+    gameId: number,
+    timeoutInterval: number,
+  ) => Promise<PendingTransaction>;
+
+  changeNumberOfDevicesOnChain: (
+    client: Client,
+    address: string,
+    gameId: number,
+    numberOfDevices: number,
   ) => Promise<PendingTransaction>;
 }
 
@@ -73,6 +100,98 @@ export const useRegisterStore = create<
           UInt64.from(discount),
           UInt64.from(timeoutInterval),
           UInt64.from(number_of_devices_allowed),
+        );
+      });
+
+      await tx.sign();
+      await tx.send();
+
+      isPendingTransaction(tx.transaction);
+      return tx.transaction;
+    },
+
+    async changePriceOnChain(
+      client: Client,
+      address: string,
+      gameId: number,
+      price: number,
+    ) {
+      console.log("changePriceOnChain");
+      console.table({ address, gameId, price });
+      const gameToken = client.runtime.resolve("GameToken");
+
+      const sender = PublicKey.fromBase58(address);
+
+      const tx = await client.transaction(sender, () => {
+        gameToken.setGamePrice(UInt64.from(gameId), UInt64.from(price));
+      });
+
+      await tx.sign();
+      await tx.send();
+
+      isPendingTransaction(tx.transaction);
+      return tx.transaction;
+    },
+
+    async changeDiscountOnChain(
+      client: Client,
+      address: string,
+      gameId: number,
+      discount: number,
+    ) {
+      const gameToken = client.runtime.resolve("GameToken");
+
+      const sender = PublicKey.fromBase58(address);
+
+      const tx = await client.transaction(sender, () => {
+        gameToken.setDiscount(UInt64.from(gameId), UInt64.from(discount));
+      });
+
+      await tx.sign();
+      await tx.send();
+
+      isPendingTransaction(tx.transaction);
+      return tx.transaction;
+    },
+
+    async changeTimeoutIntervalOnChain(
+      client: Client,
+      address: string,
+      gameId: number,
+      timeoutInterval: number,
+    ) {
+      const gameToken = client.runtime.resolve("GameToken");
+
+      const sender = PublicKey.fromBase58(address);
+
+      const tx = await client.transaction(sender, () => {
+        gameToken.setTimeoutInterval(
+          UInt64.from(gameId),
+          UInt64.from(timeoutInterval),
+        );
+      });
+
+      await tx.sign();
+      await tx.send();
+
+      isPendingTransaction(tx.transaction);
+      return tx.transaction;
+    },
+
+    async changeNumberOfDevicesOnChain(
+      client: Client,
+      address: string,
+      gameId: number,
+      numberOfDevices: number,
+    ) {
+      const gameToken = client.runtime.resolve("GameToken");
+
+      const sender = PublicKey.fromBase58(address);
+
+      const tx = await client.transaction(sender, () => {
+        gameToken.setNumberOfDevicesAllowed(
+          UInt64.from(gameId),
+          UInt64.from(numberOfDevices),
         );
       });
 
@@ -129,6 +248,126 @@ export const useRegisterGameOnChain = (
   ]);
 };
 
+export const useChangePriceOnChain = (gameId: number, price: number) => {
+  const gameRegisterStore = useRegisterStore();
+  const client = useClientStore();
+  const userStore = useUserStore();
+  const transactions = useTransactionStore();
+  const { toast } = useToast();
+
+  return useCallback(async () => {
+    if (userStore.isConnected === false) {
+      toast({
+        title: "Wallet not connected",
+        description: "Please connect your wallet",
+      });
+      return;
+    }
+    if (!client.client || !userStore.userPublicKey) return;
+
+    const pendingTransaction = await gameRegisterStore.changePriceOnChain(
+      client.client,
+      userStore.userPublicKey,
+      gameId,
+      price,
+    );
+
+    transactions.addPendingTransaction(pendingTransaction);
+  }, [client.client, userStore.userPublicKey, gameId, price]);
+};
+
+export const useChangeDiscountOnChain = (gameId: number, discount: number) => {
+  const gameRegisterStore = useRegisterStore();
+  const client = useClientStore();
+  const userStore = useUserStore();
+  const transactions = useTransactionStore();
+  const { toast } = useToast();
+
+  return useCallback(async () => {
+    if (userStore.isConnected === false) {
+      toast({
+        title: "Wallet not connected",
+        description: "Please connect your wallet",
+      });
+      return;
+    }
+    if (!client.client || !userStore.userPublicKey) return;
+
+    const pendingTransaction = await gameRegisterStore.changeDiscountOnChain(
+      client.client,
+      userStore.userPublicKey,
+      gameId,
+      discount,
+    );
+
+    transactions.addPendingTransaction(pendingTransaction);
+  }, [client.client, userStore.userPublicKey, gameId, discount]);
+};
+
+export const useChangeTimeoutIntervalOnChain = (
+  gameId: number,
+  timeoutInterval: number,
+) => {
+  const gameRegisterStore = useRegisterStore();
+  const client = useClientStore();
+  const userStore = useUserStore();
+  const transactions = useTransactionStore();
+  const { toast } = useToast();
+
+  return useCallback(async () => {
+    if (userStore.isConnected === false) {
+      toast({
+        title: "Wallet not connected",
+        description: "Please connect your wallet",
+      });
+      return;
+    }
+    if (!client.client || !userStore.userPublicKey) return;
+
+    const pendingTransaction =
+      await gameRegisterStore.changeTimeoutIntervalOnChain(
+        client.client,
+        userStore.userPublicKey,
+        gameId,
+        timeoutInterval,
+      );
+
+    transactions.addPendingTransaction(pendingTransaction);
+  }, [client.client, userStore.userPublicKey, gameId, timeoutInterval]);
+};
+
+export const useChangeNumberOfDevicesOnChain = (
+  gameId: number,
+  numberOfDevices: number,
+) => {
+  const gameRegisterStore = useRegisterStore();
+  const client = useClientStore();
+  const userStore = useUserStore();
+  const transactions = useTransactionStore();
+  const { toast } = useToast();
+
+  return useCallback(async () => {
+    if (userStore.isConnected === false) {
+      toast({
+        title: "Wallet not connected",
+        description: "Please connect your wallet",
+      });
+      return;
+    }
+    if (!client.client || !userStore.userPublicKey) return;
+
+    const pendingTransaction =
+      await gameRegisterStore.changeNumberOfDevicesOnChain(
+        client.client,
+        userStore.userPublicKey,
+        gameId,
+        numberOfDevices,
+      );
+
+    transactions.addPendingTransaction(pendingTransaction);
+  }, [client.client, userStore.userPublicKey, gameId, numberOfDevices]);
+};
+
 export const useObserveRegisteredGames = () => {
   const client = useClientStore();
   const { userPublicKey } = useUserStore();
@@ -153,7 +392,7 @@ export const useObserveRegisteredGames = () => {
             UInt64.from(gameId),
           );
         if (publisherAddress?.toBase58() === userPublicKey) {
-          registeredGames.push(gameId - 1);
+          registeredGames.push(gameId);
         }
       }
       registerStore.setRegisteredGames(registeredGames);
